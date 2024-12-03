c7c399a68eaf9df16ac9fc471d4cb780befe44b2
Saku Laakso <saku.laakso@solita.fi>
2024-12-03 14:08:27 +0200

    Day1 Part 2

--- day1/day1.js
+++ day1/day1.js
@@ -1,19 +1,37 @@
 const R = require('ramda');
 const { readRows, readCalibration, convertToNumber, abs } = require('../utils');
 
-const rows = readCalibration(__dirname);
+const rows = readRows(__dirname);
 
-// Part 1
-const answer1 = R.pipe(
+// Util
+const convertToNumberArrays = R.pipe(
     R.map(R.pipe(
         R.split(" "),
         R.filter(R.complement(R.isEmpty)),
         R.map(convertToNumber),
     )),
-    R.transpose,
+    R.transpose
+)
+
+// Part 1
+const answer1 = R.pipe(
+    convertToNumberArrays,
     R.map(R.sort(R.ascend(R.dec))),
     R.apply(R.zipWith(abs)),
     R.sum
 )(rows)
 
 console.log(answer1);
+
+// Part 2
+
+const reducer = R.curry((x, prev, current) => R.ifElse(R.equals(x), R.add(prev), R.always(prev))(current))
+const findEquals = (list1, list2) => R.map(x => R.reduce(reducer(x), 0)(list2))(list1)
+
+const answer2 = R.pipe(
+    convertToNumberArrays,
+    R.apply(findEquals),
+    R.sum
+)(rows)
+
+console.log(answer2)
